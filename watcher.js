15d8b9226b7d8d7e7ba3984b3866ca4bfbaa1f13
hehaijin <hehaijin1981@gmail.com>
2018-05-17 12:00:08 -0600

    adding parameter, string interpolation

--- watcher.js
+++ watcher.js
@@ -1,5 +1,12 @@
 'use strict';
 
 const fs=require('fs')
+const filename= process.argv[2];
+
+if(!filename) {
+	
+	throw Error('A file to watch must be specified');
+}
+
 fs.watch('target.txt',()=> console.log('File Changed'));
-console.log("start watching target.txt for changes ...");
+console.log(`start watching ${filename} for changes ...`);
